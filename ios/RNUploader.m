3c8f05e94d3dff42bbfc3875bfc7d460a10a51a2
Zak James <zak.james@gmail.com>
2018-01-18 14:11:03 -0500

    crop image before resize

--- ios/RNUploader.m
+++ ios/RNUploader.m
@@ -152,9 +152,9 @@ RCT_EXPORT_METHOD(upload:(NSDictionary *)obj callback:(RCTResponseSenderBlock)ca
                 }else if([MIMEType rangeOfString:@"image" options:NSRegularExpressionSearch].location != NSNotFound){
 
                     CGImageRef fullScreenImageRef = [rep fullScreenImage];
-                    UIImage *image = [self _square_resize: fullScreenImageRef];
-
-                    //[UIImage imageWithCGImage:fullScreenImageRef];
+                    CGSize size = CGSizeMake(750, 750);
+                    UIImage *croppedImage = [self imageByCroppingImage:[UIImage imageWithCGImage:fullScreenImageRef] toSize: size];
+                    UIImage *image = [self _square_resize: croppedImage.CGImage];
 
                     //always send jpeg
                     NSData *jpgImageData = UIImageJPEGRepresentation(image, 0.7);
@@ -229,6 +229,42 @@ RCT_EXPORT_METHOD(upload:(NSDictionary *)obj callback:(RCTResponseSenderBlock)ca
     CGImageRelease(imageRef);
 
     return returnImage;
+}
+
+- (UIImage *)imageByCroppingImage:(UIImage *)image toSize:(CGSize)size
+{
+    double newCropWidth, newCropHeight;
+
+    //=== To crop more efficently =====//
+    if(image.size.width < image.size.height){
+        if (image.size.width < size.width) {
+            newCropWidth = size.width;
+        }
+        else {
+            newCropWidth = image.size.width;
+        }
+        newCropHeight = (newCropWidth * size.height)/size.width;
+    } else {
+        if (image.size.height < size.height) {
+            newCropHeight = size.height;
+        }
+        else {
+            newCropHeight = image.size.height;
+        }
+        newCropWidth = (newCropHeight * size.width)/size.height;
+    }
+    //==============================//
+
+    double x = image.size.width/2.0 - newCropWidth/2.0;
+    double y = image.size.height/2.0 - newCropHeight/2.0;
+
+    CGRect cropRect = CGRectMake(x, y, newCropWidth, newCropHeight);
+    CGImageRef imageRef = CGImageCreateWithImageInRect([image CGImage], cropRect);
+
+    UIImage *cropped = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+
+    return cropped;
 }
 
 - (void)appendFiles {
